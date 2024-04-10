979568dc1a7b2a22a9cef7215fae8a1ad56ff0db
amater-w <2908834454@qq.com>
2024-04-10 20:24:29 +0800

    update

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -5,14 +5,21 @@
 //
 // Execute `rustlings hint if2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 pub fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
     } else {
-        1
+        if fizzish=="literally anything" {
+            "baz"
+        }else{
+            "bar"
+        }
+
     }
+
+
 }
 
 // No test changes needed!
